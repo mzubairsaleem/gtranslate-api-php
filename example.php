a5a3fd2fa38dba6bc0ea9630d6b1dc80c54a2440
josedasilva <josedasilva@1d858b06-e02a-11dd-ab27-75d9513d8d6c>
2010-12-31 17:54:33 +0000

    Uploading version 0.7.7, icnludes several issue fixing

--- example.php
+++ example.php
@@ -14,6 +14,8 @@ $translate_string = "Das ist wunderschön";
 	*/
 	$gt->setRequestType('curl');
 	echo "[CURL] Translating [$translate_string] German to English => ".$gt->german_to_english($translate_string)."\n";
+	$translate_string	=	'hello';
+	echo "[CURL] Translating [$translate_string] English to German=> ".$gt->english_to_german($translate_string)."\n";
 
 } catch (GTranslateException $ge)
  {
